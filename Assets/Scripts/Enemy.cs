91d71bb34ceb117ce72edf7becffc149a2685b26
MurkesM <ethanmartin377@yahoo.com>
2022-04-13 21:31:12 -0500

    Added a basic enemy and laser

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _enemySpeed = 4;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        if (transform.position.y <= -5.80f)
+        {
+            float randomXPosition = Random.Range(-9f, 9f);
+            transform.position = new Vector3(randomXPosition, 8, 0);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            
+            if (player != null)
+            {
+                player.Damage();
+            }
+
+            Destroy(gameObject);
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   [SerializeField] private float _playerSpeed = 3.5f;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private int _playerLives = 3;
+    [SerializeField] private float _playerSpeed = 3.5f;
+    [SerializeField] private float _fireRate = 0.15f;
+    private float _canFire = -1f;
 
     void Start()
     {
@@ -14,6 +18,9 @@ public class Player : MonoBehaviour
     void Update()
     {
         HandleMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= _canFire)
+            ShootLaser();
     }
 
     void HandleMovement()
@@ -23,13 +30,30 @@ public class Player : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        transform.Translate(direction * _playerSpeed * Time.deltaTime);
+        transform.Translate(_playerSpeed * Time.deltaTime * direction);
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.96f, 0), 0);
 
         if (transform.position.x <= -11.4f)
             transform.position = new Vector3(11.4f, transform.position.y, 0);
+        
         else if (transform.position.x >= 11.4f)
             transform.position = new Vector3(-11.4f, transform.position.y, 0);
+    }
+
+    void ShootLaser()
+    {
+        Vector3 laserOffset = new Vector3(0, 0.8f, 0);
+               
+        Instantiate(_laserPrefab, transform.position + laserOffset, Quaternion.identity);
+        _canFire = Time.time + _fireRate;
+    }
+
+    public void Damage()
+    {
+        _playerLives--;
+
+        if (_playerLives < 1)
+            Destroy(gameObject);
     }
 }
